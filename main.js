7ce055115c5395abd6cb59b838d0ffb404367d31
uchesylvester <okorocode@gmial.com>
2019-03-09 20:01:04 +0100

    added random logic

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('working')
+// console.log('working')
+
+const endPoint = 'https://www.jsonstore.io/518caa2abc1f8994b074f6e2e1d559122e8b1ee1e24b21a041fbf57a91aa629d';
+
+// function getRandom() {
+//     const randomString = Math.random().toString(32).substring(2, 5) + 
+//         Math.random().toString(32).substring(2, 5); 
+//     return randomString;
+// }
+
+function getRandom() {
+    let text = "";
+    const possibleText = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for(let i = 0; i < 5; i++) {
+
+        text += possibleText.charAt(Math.random)
+        
+    }
+}
